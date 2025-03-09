5edc3394d4e2c3bf88861cf059a98072dc905ad7
PJMSGIT <pablojavier851@gmail.com>
2025-03-09 12:04:37 +0000

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -12,13 +12,13 @@
     <title>Thumbnail Gallery - Start Bootstrap Template</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- Custom CSS -->
-    <link href="/assets/bootstrap/css/thumbnail-gallery.css" rel="stylesheet">
+    <link href="../assets/bootstrap/css/thumbnail-gallery.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="/assets/css/estilos.css" rel="stylesheet">
+    <link href="../assets/css/estilos.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -81,10 +81,10 @@
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="/assets/bootstrap/js/jquery.js"></script>
+    <script src="../assets/bootstrap/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
 
 </body>
 
